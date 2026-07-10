456ac021319422826be0cf679e946dffe6e8789c
이현진 <zse846@gmail.com>
2026-07-10 17:12:46 +0900

    플레이어 네트워크 연동
    이동, 데이터가 서버를 통해서 동기화됨

--- Assets/Scripts/Player/PlayerData.cs
+++ Assets/Scripts/Player/PlayerData.cs
@@ -1,30 +1,30 @@
+using Unity.Netcode;
 using UnityEngine;
-// using Unity.Netcode; // TODO: 네트워크 테스트 시 주석 해제
 
-public class PlayerData : MonoBehaviour // TODO: 네트워크 테스트 시 NetworkBehaviour로 복구
+public class PlayerData : NetworkBehaviour
 {
     [Header("스테이터스")]
     [SerializeField] private int m_maxHp = 100;
 
-    // TODO: 네트워크 테스트 시 아래 두 프로퍼티를 NetworkVariable<int>로 교체
-    // (지금처럼 로컬 프로퍼티면 다른 클라이언트에 값이 동기화 안 됨)
-    public int PlayerId { get; private set; } // TODO: OwnerClientId로 대체 가능한지 검토
+    private readonly NetworkVariable<int> m_currentHp = new NetworkVariable<int>();
+
+    public ulong PlayerId => OwnerClientId;
     public int MaxHp => m_maxHp;
-    public int CurrentHp { get; private set; }
+    public int CurrentHp => m_currentHp.Value;
 
-    private void Awake()
+    public override void OnNetworkSpawn()
     {
-        CurrentHp = m_maxHp;
+        if (IsServer)
+        {
+            m_currentHp.Value = m_maxHp;
+        }
     }
 
-    public void SetPlayerId(int playerId)
-    {
-        PlayerId = playerId;
-    }
-
-    // TODO: 네트워크 테스트 시 서버 권위로만 호출되게 변경 (ServerRpc로 요청 → 서버가 실제 값 변경)
+    // 서버 권위로만 실제 값 변경. (데미지 소스가 클라라면 별도 ServerRpc로 요청)
     public void ModifyHp(int delta)
     {
-        CurrentHp = Mathf.Clamp(CurrentHp + delta, 0, m_maxHp);
+        if (!IsServer) return;
+
+        m_currentHp.Value = Mathf.Clamp(m_currentHp.Value + delta, 0, m_maxHp);
     }
 }
